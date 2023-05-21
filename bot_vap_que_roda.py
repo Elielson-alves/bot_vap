f552b8b7a58d13abef6200138bbf0ed4da0e9d1a
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2023-05-20 23:20:26 -0300

    Alguns ajustes

--- bot_vap_que_roda.py
+++ bot_vap_que_roda.py
@@ -1,19 +1,25 @@
 import discord
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
+token = os.getenv("token")
 class MyClient(discord.Client):
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        if message.content == 'Cardapio':
+        message.content = message.content.lower()
+
+        if message.content == 'cardapio':
             await message.channel.send(f'{message.author.name}, trabalhamos apenas com dois pratos:\n1 - Stroganoff de Frango\n2 - Caldo de Frango')
         elif message.content == '1':
             await message.channel.send(f'{message.author.name}, Pedido do Stroganoff de Frango em preparo.')
         elif message.content == '2':
             await message.channel.send(f'{message.author.name}, Pedido do Caldo de Frango em preparo.')
-        elif message.content == 'Pedido Pronto?':
+        elif message.content == 'pedido pronto?':
             await message.channel.send(f'{message.author.name},Pedido em preparo, aguarde.')
 
     async def on_member_join(self, member):
@@ -26,4 +32,4 @@ intents = discord.Intents.all()
 intents.members = True
 
 client = MyClient(intents=intents)
-client.run('MTEwOTYxNDkxMjEyMjEyNjM0Ng.GBD7jY.lp1LjMag0lYCCSBX_tTAeAZqgz5lYKa-dSp0Kc')
+client.run(token)
